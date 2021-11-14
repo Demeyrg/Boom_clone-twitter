5ffbc402d6f212e82f9547c00e8d8b5e4c7ad9d1
Slava Aleinikov <slava.aleinikov@mail.ru>
2021-11-15 02:25:05 +0600

    Changing mustache to freemarker Changes in the controller related to the filter

--- src/main/java/com/example/boom/controller/MainController.java
+++ src/main/java/com/example/boom/controller/MainController.java
@@ -6,6 +6,7 @@ import com.example.boom.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,9 +25,18 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
-        Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages",messages);
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
+
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages",messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -39,20 +49,10 @@ public class MainController {
         Message message = new Message(text, tag, user);
         messageRepo.save(message);
 
+
+
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages",messages);
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String tag, Map<String, Object> model) {
-        Iterable<Message> messages;
-        if (tag != null && !tag.isEmpty()) {
-            messages = messageRepo.findByTag(tag);
-        } else {
-            messages = messageRepo.findAll();
-        }
-        model.put("messages", messages);
         return "main";
     }
 }
