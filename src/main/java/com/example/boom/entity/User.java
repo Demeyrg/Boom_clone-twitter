f37a0701a3e3653ac55572e97a4e5007b6ab4d0a
Slava Aleinikov <slava.aleinikov@mail.ru>
2021-11-17 20:38:51 +0600

    Start UI with Bootstrap Added navigation bar (navbar.ftlh) Used in common.ftlh. Change login and regist pages

--- src/main/java/com/example/boom/entity/User.java
+++ src/main/java/com/example/boom/entity/User.java
@@ -26,6 +26,10 @@ public class User implements UserDetails {
     public User() {
     }
 
+    public boolean isAdmin() {
+        return roles.contains(Role.ADMIN);
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return getRoles();
