856e8092038e76baff553d440a222b8531994a92
Slava Aleinikov <slava.aleinikov@mail.ru>
2021-11-19 21:22:23 +0600

    Mail distribution: Added fields to the User class - email and activationCode Added MailConfig Added MailSender service

--- src/main/java/com/example/boom/controller/RegistrationController.java
+++ src/main/java/com/example/boom/controller/RegistrationController.java
@@ -1,21 +1,19 @@
 package com.example.boom.controller;
 
-import com.example.boom.entity.Role;
 import com.example.boom.entity.User;
-import com.example.boom.repository.UserRepository;
+import com.example.boom.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Collections;
-import java.util.Map;
 
 @Controller
 public class RegistrationController {
 
     @Autowired
-    private UserRepository userRepo;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration() {
@@ -23,18 +21,26 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> model) {
-        User userFromDb = userRepo.findByUsername(user.getUsername());
-
-        if (userFromDb != null) {
-            model.put("message","User exists!");
+    public String addUser(User user, Model model) {
+        if (!userService.addUser(user)) {
+            model.addAttribute("message", "User exists!");
             return "registration";
         }
 
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepo.save(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(@PathVariable String code, Model model) {
+        boolean isActivated = userService.activateUser(code);
+
+        if(isActivated) {
+            model.addAttribute("message", "User successfully activated");
+        } else {
+            model.addAttribute("message", "Activation code is not found!");
+        }
+
+        return "login";
     }
 
 }
