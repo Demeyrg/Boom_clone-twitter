0e88f8b09fa9baee95ac24c6e33cdc87b53a2f0d
Slava Aleinikov <slava.aleinikov@mail.ru>
2021-12-04 23:37:08 +0600

    Changing the migration database 3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,13 @@ mvn clean package
 
 echo 'Copy files...'
 
-scp -i ~/.ssh/authorized_keys \
-    target/sweater-1.0-SNAPSHOT.jar \
-    slava@46.137.15.86:/home/dru/
+scp -i ~/.ssh/authorized-key \
+    target/boom-1.0-SNAPSHOT.jar \
+    slava@18.193.191.240:/home/dru/
 
 echo 'Restart server...'
 
-ssh -i ~/.ssh/id_rsa_drucoder dru@46.137.15.86 << EOF
+ssh -i ~/.ssh/authorized-key dru@18.193.191.240 << EOF
 pgrep java | xargs kill -9
 nohup java -jar sweater-1.0-SNAPSHOT.jar > log.txt &
 EOF
--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -1,7 +1,9 @@
 create sequence hibernate_sequence start 1 increment 1;
 
+create if not exists schema boom;
+
 create table message (
-    id int8 not null,
+    id serial not null,
     filename varchar(255),
     tag varchar(255),
     text varchar(3000) not null,
@@ -10,12 +12,12 @@ create table message (
 );
 
 create table user_role (
-    user_id int8 not null,
+    user_id serial not null,
     roles varchar(255)
 );
 
 create table usr (
-    id int8 not null,
+    id serial not null,
     activation_code varchar(255),
     active boolean not null,
     email varchar(255),
@@ -24,14 +26,10 @@ create table usr (
     primary key (id)
 );
 
-alter table if exists message
+alter table if exists boom.message
     add constraint message_user_fk
     foreign key (user_id) references usr;
 
-alter table if exists user_role
+alter table if exists boom.user_role
     add constraint user_role_user_fk
     foreign key (user_id) references usr;
-
-insert into usr (username, password, active) values ('admin','admin',true);
-
-insert into user_role (user_id, roles) values ((select id from usr where username = 'admin'),'USER'), ((select id from usr where username = 'admin'),'ADMIN');
